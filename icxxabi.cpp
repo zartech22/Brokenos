47d0055404c2484e3c8b4e115da3cfc9b258da63
zartech <kevin.plestan@outlook.fr>
2016-05-10 03:11:53 +0200

    Add stack check support

--- icxxabi.cpp
+++ icxxabi.cpp
@@ -24,6 +24,14 @@ int __cxa_atexit(void (*f)(void *), void *objptr, void *dso)
 	__atexit_func_count++;
 	return 0; /*I would prefer if functions returned 1 on success, but the ABI says...*/
 };
+
+void __stack_chk_fail()
+{
+    Screen::getScreen().clean();
+    Screen::getScreen().printError("Stack check failed !");
+
+    asm("hlt");
+}
  
 void __cxa_finalize(void *f)
 {
